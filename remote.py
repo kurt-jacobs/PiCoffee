b364e9b2c2bd01a4f011b1cf5d39518f95ca06b4
kurt-jacobs <kurt.jacobs@gmail.com>
2014-08-16 23:31:58 -0500

    removed unnecessary includes

--- remote.py
+++ remote.py
@@ -1,12 +1,8 @@
 import flask, flask.views
-import RPi.GPIO as GPIO, time
-import os
-import coffeeUtils
 
 class Remote(flask.views.MethodView):
 
     def __init__(self):       
-        self.coffeeIO=coffeeUtils.CoffeeIO()
         return
 
     def get(self):
